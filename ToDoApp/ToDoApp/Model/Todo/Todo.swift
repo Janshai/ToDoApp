89076163b6588be18ed22ba5bb18ab2ae01bb9fa
Janshai <jack.r.s.adams@gmail.com>
2019-08-18 16:24:13 +0100

    add categories to todo class

--- ToDoApp/ToDoApp/Model/Todo/Todo.swift
+++ ToDoApp/ToDoApp/Model/Todo/Todo.swift
@@ -10,18 +10,23 @@ import Foundation
 
 class Todo: Codable {
     public var title: String
+    public private(set) var categories: [String]
     public private(set) var id: String
     public private(set) var createdAt: Date
     
-    init(title: String, id: String? = nil, createdAt: Date? = nil) {
+    init(title: String, categories: [String] = [], id: String? = nil, createdAt: Date? = nil) {
         self.title = title
         self.id = id ?? UUID().uuidString
         self.createdAt = createdAt ?? Date()
+        self.categories = categories
     }
     
     private enum CodingKeys: String, CodingKey {
         case title
+        case categories
         case id = "_id"
         case createdAt
     }
+    
+    
 }
