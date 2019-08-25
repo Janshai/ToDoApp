617e146c13d58a6f747767d1cba28c4d933e5aa4
Janshai <jack.r.s.adams@gmail.com>
2019-08-25 01:54:58 +0100

    Deleted surplus files and added documentation to MMViewController

--- ToDoApp/ToDoApp/View/VCs/MainMenuViewController.swift
+++ ToDoApp/ToDoApp/View/VCs/MainMenuViewController.swift
@@ -9,16 +9,21 @@
 import UIKit
 
 class MainMenuViewController: UIViewController {
-
+    // MARK: Segue Identifiers
     private let todoScreenSegue = "TodoScreen"
     private let CategorySegue = "Category"
     
+    //MARK: Category Model related properties
     private var categoryModelController = CategoryModelController()
     private var categoryViewModels = [CategoryViewModel]()
     
+    //MARK: Outlets
+    /// Outlet for Main TableView
     @IBOutlet weak var tableView: UITableView!
     
     
+    //MARK VC Lifecycle Functions
+    /// Override of ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -29,12 +34,17 @@ class MainMenuViewController: UIViewController {
         
     }
     
+    
+    /// Override of Preparation for Segues function
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
+        
         if segue.identifier == todoScreenSegue, let display = sender as? TaskDisplay, let todoVC = segue.destination as? TasksTableViewController {
+            // segue to Tasks screen
             todoVC.displaying = display
             todoVC.categoryModelController = self.categoryModelController
         } else if segue.identifier == CategorySegue, let vc = segue.destination as? CategoryViewController {
+            // segue to add a new category or edit an existing category
 //            if let category = sender as? Category {
 //                vc.function = .edit
 //                vc.category = category
@@ -48,10 +58,17 @@ class MainMenuViewController: UIViewController {
 
 extension MainMenuViewController: UITableViewDelegate, UITableViewDataSource {
     
+    //MARK: TableView Data Source Functions
+    
+    /// Number of Sections in the TableView
+    /// Should be 2
     func numberOfSections(in tableView: UITableView) -> Int {
         return 2
     }
     
+    /// Number of rows in each section
+    /// - First Section has one row for segueing to display all todos
+    /// - Second Section has as many rows as there are Categories
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         switch section {
         case 0:
@@ -64,6 +81,9 @@ extension MainMenuViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     
+    /// Cell For Row In Section
+    /// - Section 1 has 1 cell for displaying all todos
+    /// - Section 2 cells describe the categories of tasks. Setup can be seen in TaskTableView.swift
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! TaskTableViewCell
@@ -83,6 +103,11 @@ extension MainMenuViewController: UITableViewDelegate, UITableViewDataSource {
         return cell
     }
     
+    // MARK: TableView Delegate Functions
+    
+    /// Behaviour for selecting cells
+    /// All cells segue to the tasks screen in TasksTableViewController.swift and Main.storyboard.
+    /// The categories cells filter the tasks displayed on the next screen to just tasks in the selected category
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         var displayType: TaskDisplay
         switch indexPath.section {
@@ -97,6 +122,8 @@ extension MainMenuViewController: UITableViewDelegate, UITableViewDataSource {
         
     }
     
+    
+    /// Swiping Right on a Category presents an option to delete it. Setup of this is in TasksTableView.swift
     func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
         
         switch indexPath.section {
@@ -108,6 +135,8 @@ extension MainMenuViewController: UITableViewDelegate, UITableViewDataSource {
         
     }
     
+    /// Swiping Left on a Category presents an option to edit it. Setup of this is in TasksTableView.swift
+    /// If the user edits the category, we segue to the Category Screen in CategoryViewController.swift and Category.storyboard
     func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
         
         switch indexPath.section {
@@ -121,6 +150,9 @@ extension MainMenuViewController: UITableViewDelegate, UITableViewDataSource {
         }
     }
     
+    /// Setup the headers for the TableView
+    /// First Section has no header
+    /// Second Section Header is created in setupHeader(forTableView:)
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         switch section {
         case 1:
@@ -131,10 +163,13 @@ extension MainMenuViewController: UITableViewDelegate, UITableViewDataSource {
         
     }
     
+    /// Header height
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 35
     }
     
+    /// Header for categories section
+    /// Has a + button that when pressed causes a segue to the Add Category screen described in CategoryViewController.swift and Category.storyboard
     private func setupCategoriesHeader(forTableView tableView: UITableView) -> UIView? {
         
         let view = tableView.dequeueReusableCell(withIdentifier: "header") as! MMTableViewHeader
